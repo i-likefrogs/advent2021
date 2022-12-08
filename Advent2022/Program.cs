d43cd8d9c033e3f123c7f866e79de92f927885f8
i-likefrogs <120089694+i-likefrogs@users.noreply.github.com>
2022-12-08 19:54:30 +1100

    Day 1 - 1

---
+++ Advent2022/Program.cs
@@ -0,0 +1,45 @@
+﻿// chunk
+
+var max = Extensions
+    .LoadFile("day_1_input.txt")
+    .Chunk(string.IsNullOrEmpty)
+    .Select(a => a.Sum(int.Parse)).Max();
+
+Console.WriteLine(max);
+
+
+public static class Extensions
+{
+    public static IEnumerable<string> LoadFile(string name)
+    {
+        return File.ReadLines(Path.Combine(Environment.CurrentDirectory, "files", name));
+    }
+
+    public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, Func<T, bool> chunkOn,
+        bool discardChunkMarker = true)
+    {
+        var list = new List<T>();
+
+        foreach (var sourceItem in source)
+        {
+            var chunkMark = chunkOn(sourceItem);
+            if (!chunkMark)
+            {
+                list.Add(sourceItem);
+            }
+            else
+            {
+                yield return list;
+
+                list = new List<T>();
+
+                if (!discardChunkMarker)
+                {
+                    list.Add(sourceItem);
+                }
+            }
+        }
+
+        yield return list;
+    }
+}
